c623e98dd14dee7bdf55713c12d39dc3b9decf3e
Winnux Reboxyz <reboxyz@gmail.com>
2024-02-12 18:06:07 +0900

    Toggling editing state with Alpine.js

---
+++ resources/views/livewire/edit-post.blade.php
@@ -0,0 +1,15 @@
+<form wire:submit='create' class="space-y-2">
+    <div>
+        <label for="body" class="sr-only">Post body</label> 
+        <x-textarea-input class="w-full p-4" 
+            wire:model="" 
+        />
+        <x-input-error :messages="$errors->get('body')" />
+    </div>
+
+    <div class="flex items-center space-x-2">
+        <x-primary-button>Edit</x-primary-button>
+        {{-- Note! alpine's dispatch event --}}
+        <button type="button" x-on:click="$dispatch('edit-cancel')">Cancel</button>
+    </div>        
+</form>
--- resources/views/livewire/post-item.blade.php
+++ resources/views/livewire/post-item.blade.php
@@ -4,15 +4,29 @@
     </div>
     <div class="space-y-2 grow">
         <div class="text-lg font-bold">{{ $post->id }} {{ $post->user->name }}</div>
-        <div>
-            <p>{{ $post->body }}</p>
-        </div>
-        <div class="flex items-center space-x-2">
-            @can('delete', $post)
-                <div>
-                    <button class="text-indigo-500" wire:click="deletePost({{ $post->id}})">Delete</button>
+        {{-- Note! In alpine.js, define an 'editing' boolean variable  --}}
+        <div x-data="{ editing: false }" x-on:edit-cancel="editing = false">
+            <div class="space-y-4" x-show="!editing">
+                <p>{{ $post->body }}</p>
+            
+                <div class="flex items-center space-x-2">
+                    @can('edit', $post)
+                        <div>
+                            <button class="text-indigo-500" x-on:click="editing = true">Edit</button>
+                        </div>
+                    @endcan
+
+                    @can('delete', $post)
+                        <div>
+                            <button class="text-indigo-500" wire:click="deletePost({{ $post->id}})">Delete</button>
+                        </div>
+                    @endcan
                 </div>
-            @endcan
+            </div>
+            {{-- Note! x-cloak is defined is app.css which is to eliminate flickering --}}
+            <div x-show="editing" x-cloak>
+                <livewire:edit-post :post="$post">
+            </div>
         </div>
     </div>
 </div>
